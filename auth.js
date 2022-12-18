d52d22dc20d73c631b9d4b42a3b7e6c287410ad1
your_name <mak30597@gmail.com>
2022-12-18 23:27:59 +0500

    commit

--- auth.js
+++ auth.js
@@ -1,11 +1,34 @@
-const express=require('express')
 
+const express=require('express')
 const router=express.Router();
+require('./conn')
+const User=require("./userSchema")
 
 router.post('/register',(req,res)=>{
     
-    console.log(req.body)
-    res.json({message:req.body});
+
+const{name,email,work,password,cpassword}=req.body;
+
+if(!name ||!email|| !work|| !password ||!cpassword){
+    return res.json({error:"Please fill All"});
+} 
+
+User.findOne({email:email}).then((userExists)=>{
+    if(userExists){
+        return res.json({error:"Please user different email "})
+    }
+    const user=new User(req.body)
+    user.save().then(()=>{
+        res.json({message:"Data-Saved"})
+    }).catch((err)=>res.json({error:"Failed to register data"}))
+})
+ 
+//console.log(req.body)
+ //res.json({message:req.body});
+
+
+
+
 });
 
 module.exports=router;
