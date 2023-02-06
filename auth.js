9fa876cc03ffa78097fddef49886bfa9e73db672
your_name <mak30597@gmail.com>
2023-02-06 09:36:14 +0500

    bcrypt

--- auth.js
+++ auth.js
@@ -19,16 +19,45 @@ try{
          return res.json({error:"Please use diff email "})
      }
      const user =new User({name,email,work,password,cpassword});
+
+     
      await user.save();
      res.json({message:"User Register "})
 
 }catch(err){
     console.log(err)
 }
- 
-//console.log(req.body)
- //res.json({message:req.body});
 
 });
+
+
+router.post('/signin',async(req,res)=>{
+try{
+
+const {email,password}=req.body;
+
+if(!email||!password){
+    return res.json({error:"please fill the data"})
+}
+const userLogin=await User.findOne({email:email});
+console.log(userLogin);
+if(!userLogin){
+    res.json({error:"user error"});
+}
+else{
+    res.json({message:"user Signin Successfully"})
+}
+
+
+}catch(err){
+    console.log(err);
+}
+
+
+
+
+});
+
+ 
 
 module.exports=router;
