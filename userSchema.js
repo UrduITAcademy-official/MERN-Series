c3060089c560029cf6cc42d8afc11e71ac05347e
your_name <mak30597@gmail.com>
2023-02-06 09:36:30 +0500

    bcrypt

--- userSchema.js
+++ userSchema.js
@@ -1,4 +1,7 @@
 const mongoose=require('mongoose');
+
+const bcrypt=require('bcrypt')
+
 const userSchema=new mongoose.Schema({
 name:{
 
@@ -9,7 +12,6 @@ email:{
     type:String,
     require:true
 },
-
 phone:{
     type:Number,
     require:true
@@ -32,6 +34,17 @@ cpassword:{
 
 },
 })
+userSchema.pre('save',async function(next){
+
+if(this.isModified('password'))
+{     this.password=await bcrypt.hash(this.password,12); 
+    this.cpassword=await bcrypt.hash(this.cpassword,12);
+
+}
+next();
+});
+
+
 
 const User=mongoose.model('USER',userSchema);
 module.exports=User;
