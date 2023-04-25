10e8317afb0c21ca281170b4064e2e768467b75a
your_name <mak30597@gmail.com>
2023-04-25 20:54:39 +0500

    working JWT

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
-
+import { useNavigate } from "react-router-dom";
 const About = () => {
+  const history = useNavigate();
   const callAboutPage = async () => {
     try {
       // Get the cookie value from the browser
@@ -8,7 +9,6 @@ const About = () => {
         .split(";")
         .find((cookie) => cookie.trim().startsWith("jwtauth"));
       const cookieValue = cookie ? cookie.split("=")[1] : null;
-
       // Send a GET request to the backend with the cookie as a header
       const response = await fetch("/about", {
         headers: {
@@ -16,12 +16,17 @@ const About = () => {
         },
       });
       const data = await response.json();
-      console.log(data);
+      if (data.error) {
+        console.log("hi from if");
+        history("/login");
+      } else {
+        console.log(data);
+        console.log("hi from elase");
+      }
     } catch (error) {
       console.error(error);
     }
   };
-
   useEffect(() => {
     // This function will be called after the component mounts
     // and every time the data state changes
