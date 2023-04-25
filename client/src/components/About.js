ee0997f9b73a890e713b8816ef9cd34ef87a00c7
your_name <mak30597@gmail.com>
2023-04-25 19:28:59 +0500

    working JWT

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,32 +1,31 @@
 import React, { useEffect } from "react";
 
 const About = () => {
-    const callAboutPage = async () => {
-        try {
-            // Get the cookie value from the browser
-            const cookie = document.cookie.split(';').find(cookie => cookie.trim().startsWith('jwtauth'));
-            const cookieValue = cookie ? cookie.split('=')[1] : null;
-        
-            // Send a GET request to the backend with the cookie as a header
-            const response = await fetch('/about', {
-              headers: {
-                'Cookie': `jwtauth=${cookieValue}`
-              }
-            });
-            const data = await response.json();
-            console.log(data);
-          } catch (error) {
-            console.error(error);
-          }
+  const callAboutPage = async () => {
+    try {
+      // Get the cookie value from the browser
+      const cookie = document.cookie
+        .split(";")
+        .find((cookie) => cookie.trim().startsWith("jwtauth"));
+      const cookieValue = cookie ? cookie.split("=")[1] : null;
 
-
+      // Send a GET request to the backend with the cookie as a header
+      const response = await fetch("/about", {
+        headers: {
+          Cookie: `jwtauth=${cookieValue}`,
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
     }
-      
-      
+  };
+
   useEffect(() => {
     // This function will be called after the component mounts
     // and every time the data state changes
-   callAboutPage();
+    callAboutPage();
   }, []);
 
   return (
