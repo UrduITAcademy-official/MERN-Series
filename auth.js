2cfb4497c9a3b16dcfc5313239351f85a3dd66af
your_name <mak30597@gmail.com>
2023-06-17 14:35:26 +0500

    login,register,and session on page working

--- auth.js
+++ auth.js
@@ -1,91 +1,71 @@
+const express = require("express");
+const router = express.Router();
+const authenticate = require("./middleware/authenticate");
+const jwt = require("jsonwebtoken");
+require("./conn");
+const User = require("./userSchema");
+const bcrypt = require("bcrypt");
+const { now } = require("mongoose");
 
-const express=require('express')
-const router=express.Router();
-const authenticate=require("./middleware/authenticate")
-const jwt=require('jsonwebtoken');
-require('./conn')
-const User=require("./userSchema")
-const bcrypt =require('bcrypt');
-const { now } = require('mongoose');
-
-router.post('/register',async(req,res)=>{
-    
-const{name,email,work,password,cpassword}=req.body;
-
-if(!name ||!email|| !work|| !password ||!cpassword){
-    return res.json({error:"Please fill All"});
-} 
-
-try{
-     const userExists=await User.findOne({email:email})
-
-     if(userExists){
-         return res.json({error:"Please use diff email "})
-
-        
-     }
-     const user =new User({name,email,work,password,cpassword});
-
-
-     await user.save();
-     res.json({message:"User Register "})
-
-}catch(err){
-    console.log(err)
-}
-
-});
-
-
-router.post('/signin',async(req,res)=>{
-try{
-const {email,password}=req.body;
-if(!email||!password){
-    return res.status(400).json({error:"please fill the data"})
-}
-const userLogin=await User.findOne({email:email});
-//console.log(userLogin);
-if(!userLogin){
-    res.status(400).json({error:"user error"});
-}
-else{
-    //res.json({message:"user Signin Successfully Comparing password"})
-    const isMatch=await bcrypt.compare(password,userLogin.password);{
-        if(isMatch)
-        {
-            //res.json({message:"Password is Matched"})
-            const token = await userLogin.genrateAuthToken();
-            console.log(token);
-           res.cookie('jwtauth',token,{
-               expires:new Date(Date.now()+25892000000),
-               httpOnly:true
-           }).setHeader('message', 'Cookie set successfully')
-           .sendStatus(200).send();
-        
-        
-
-        }
-        else{
-            res.status(400).json({message:"Password is Not matched with hash"})
-        }
-
-}}
-
-
-}catch(err){
+router.post("/register", async (req, res) => {
+  const { name, email, work, password, cpassword } = req.body;
+  if (!name || !email || !work || !password || !cpassword) {
+    return res.json({ error: "Please fill All" });
+  }
+  try {
+    const userExists = await User.findOne({ email: email });
+    if (userExists) {
+      return res.json({ error: "Please use diff email " });
+    }
+    const user = new User({ name, email, work, password, cpassword });
+    await user.save();
+    res.json({ message: "User Register " });
+  } catch (err) {
     console.log(err);
-}
-
-
-
-
+  }
 });
 
-router.get('/about',authenticate,(req,res)=>{
-   res.send(req.rootUser);
-   console.log('Hello from about page')
+router.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ error: "please fill the data" });
+    }
+    const userLogin = await User.findOne({ email: email });
+    //console.log(userLogin);
+    if (!userLogin) {
+      res.status(400).json({ error: "user error" });
+    } else {
+      //res.json({message:"user Signin Successfully Comparing password"})
+      const isMatch = await bcrypt.compare(password, userLogin.password);
+      {
+        if (isMatch) {
+          //res.json({message:"Password is Matched"})
+          const token = await userLogin.genrateAuthToken();
+          console.log(token);
+          res
+            .cookie("jwtauth", token, {
+              expires: new Date(Date.now() + 25892000000),
+              httpOnly: true,
+            })
+            .setHeader("message", "Cookie set successfully")
+            .sendStatus(200)
+            .send();
+        } else {
+          res
+            .status(400)
+            .json({ message: "Password is Not matched with hash" });
+        }
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
- 
+router.get("/about", authenticate, (req, res) => {
+  res.send(req.rootUser);
+  console.log("Hello from about page");
+});
 
-module.exports=router;
+module.exports = router;
--- userSchema.js
+++ userSchema.js
@@ -1,74 +1,59 @@
-const mongoose=require('mongoose');
-const jwt=require('jsonwebtoken');
-const bcrypt=require('bcrypt')
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  phone: {
+    type: Number,
+    require: true,
+  },
 
-const userSchema=new mongoose.Schema({
-name:{
-
-    type:String,
-    require:true
-},
-email:{
-    type:String,
-    require:true
-},
-phone:{
-    type:Number,
-    require:true
-},
-
-work:{
-
-    type:String,
-    require:true
-},
-password:{
-
-    type:String,
-    require:true
-},
-cpassword:{
-
-    type:String,
-    require:true
-
-},
-tokens:[
+  work: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  cpassword: {
+    type: String,
+    require: true,
+  },
+  tokens: [
     {
-        token:{
-            type:String,
-            require:true
-        }
-    }
-]
-
-
-})
-userSchema.pre('save',async function(next){
-
-if(this.isModified('password'))
-{     this.password=await bcrypt.hash(this.password,12); 
-    this.cpassword=await bcrypt.hash(this.cpassword,12);
-
-}
-next();
+      token: {
+        type: String,
+        require: true,
+      },
+    },
+  ],
+});
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+    this.cpassword = await bcrypt.hash(this.cpassword, 12);
+  }
+  next();
 });
 
-userSchema.methods.genrateAuthToken=async function(){
+userSchema.methods.genrateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({ token: token });
+    await this.save();
+    return token;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-try{
-let token =jwt.sign({_id:this._id},process.env.SECRET_KEY);
-this.tokens=this.tokens.concat({token:token})
-await this.save();
-return token;
-
-
-}catch(err){
-    console.log(err)
-}
-
-}
-
-
-const User=mongoose.model('USER',userSchema);
-module.exports=User;
+const User = mongoose.model("USER", userSchema);
+module.exports = User;
